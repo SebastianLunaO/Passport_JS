0af6f682709ad9b30606c0a23b30d7ddde08558e
Sebastian Luna <newmsloh@gmail.com>
2025-01-04 17:57:47 -0600

    adding the session storage using MYSQL

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import mysql2 from 'mysql2/promise'
-import MySQLStore from 'express-mysql-session';
 import session from 'express-session'
-
+import MySQLSessionStoreImport from "express-mysql-session";
+const password = process.env.PASSWORD_MYSQL
+const MySQLSessionStore = MySQLSessionStoreImport(session);
 
 
 /**
@@ -11,11 +12,11 @@ import session from 'express-session'
 
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
 
-// Create the Express application
+// // Create the Express application
 var app = express();
 
-// Middleware that allows Express to parse through both JSON and x-www-form-urlencoded request bodies
-// These are the same as `bodyParser` - you probably would see bodyParser put here in most apps
+// // Middleware that allows Express to parse through both JSON and x-www-form-urlencoded request bodies
+// // These are the same as `bodyParser` - you probably would see bodyParser put here in most apps
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,81 +24,91 @@ const optionsConnection = {
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
-	password: process.env.PASSWORD_MYSQL,
-    database: 'pasport_test'
+	password: password,
+    database: 'passport_test'
 };
 
 const storeSessionOptions = {
+	createDatabaseTable: false,
     schema: {
-		tableName: 'sessions',
+		tableName: 'sessions_customs',
 		columnNames: {
-			session_id: 'session_id',
-			expires: 'expires',
-			data: 'data'
+			session_id: 'sessions_id',
+			expires: 'hash',
+			data: 'salt'
 		}
 	}
 }
 
-const connection = mysql2.createConnection(optionsConnection)
-const sessionStore = new MySQLStore ({})
+const connection = await mysql2.createConnection(optionsConnection)
 
-/**
- * -------------- DATABASE ----------------
- */
 
-/**
- * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
- * string into the `.env` file
- *
- * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
- */
+const result =  await connection.query("SELECT * FROM sessions_customs;");
 
-// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
-var UserSchema = session.getSchema('newSchema');
+const rows = result[0];
+console.log(rows)
 
-// // Defines the model that we will use in the app
-// mongoose.model("User", UserSchema);
+const storeCon = new MySQLSessionStore(storeSessionOptions,connection)
 
-/**
- * -------------- SESSION SETUP ----------------
- */
+// /**
+//  * -------------- DATABASE ----------------
+//  */
 
-/**
- * The MongoStore is used to store session data.  We will learn more about this in the post.
- *
- * Note that the `connection` used for the MongoStore is the same connection that we are using above
- */
+// /**
+//  * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
+//  * string into the `.env` file
+//  *
+//  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
+//  */
+
+// // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+// //var UserSchema = session.getSchema('newSchema');
+
+// // // Defines the model that we will use in the app
+// // mongoose.model("User", UserSchema);
+
+// /**
+//  * -------------- SESSION SETUP ----------------
+//  */
+
+// /**
+//  * The MongoStore is used to store session data.  We will learn more about this in the post.
+//  *
+//  * Note that the `connection` used for the MongoStore is the same connection that we are using above
+//  */
 // const sessionStore = new MongoStore({
 //   mongooseConnection: connection,
 //   collection: "sessions",
 // });
 
-/**
- * See the documentation for all possible options - https://www.npmjs.com/package/express-session
- *
- * As a brief overview (we will add more later):
- *
- * secret: This is a random string that will be used to "authenticate" the session.  In a production environment,
- * you would want to set this to a long, randomly generated string
- *
- * resave: when set to true, this will force the session to save even if nothing changed.  If you don't set this,
- * the app will still run but you will get a warning in the terminal
- *
- * saveUninitialized: Similar to resave, when set true, this forces the session to be saved even if it is unitialized
- */
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-  })
-);
+// /**
+//  * See the documentation for all possible options - https://www.npmjs.com/package/express-session
+//  *
+//  * As a brief overview (we will add more later):
+//  *
+//  * secret: This is a random string that will be used to "authenticate" the session.  In a production environment,
+//  * you would want to set this to a long, randomly generated string
+//  *
+//  * resave: when set to true, this will force the session to save even if nothing changed.  If you don't set this,
+//  * the app will still run but you will get a warning in the terminal
+//  *
+//  * saveUninitialized: Similar to resave, when set true, this forces the session to be saved even if it is unitialized
+//  */
+ app.use(
+   session({
+	 key: 'cookie',
+     secret: process.env.SECRET,
+     resave: false,
+     saveUninitialized: true,
+     store: storeCon,
 
-/**
- * -------------- ROUTES ----------------
- */
+ })
+ );
 
+// /**
+//  * -------------- ROUTES ----------------
+//  */
+console.log()
 // When you visit http://localhost:3000/login, you will see "Login Page"
 app.get("/login", (req, res, next) => {
   res.send("<h1>Login Page</h1>");
@@ -112,9 +123,12 @@ app.get("/register", (req, res, next) => {
 
 app.post("/register", (req, res, next) => {});
 
-/**
- * -------------- SERVER ----------------
- */
+// /**
+//  * -------------- SERVER ----------------
+//  */
 
-// Server listens on http://localhost:3000
-app.listen(3000);
+// // Server listens on http://localhost:3000
+
+app.listen(3000,()=>{
+    console.log('Server running on 3000')
+})
